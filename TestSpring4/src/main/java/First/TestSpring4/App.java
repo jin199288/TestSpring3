aaa3311f09e8a8ca714d088d56a891bd0e469dfc
jinying <jinying@git.com>
2017-08-07 17:53:07 +0800

    first commit

---
+++ TestSpring4/src/main/java/First/TestSpring4/App.java
@@ -0,0 +1,13 @@
+package First.TestSpring4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
