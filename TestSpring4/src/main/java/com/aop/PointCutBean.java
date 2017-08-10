4fd67d636b1324c297a996c19fd179d0bddf4fd9
jinying <jinying@git.com>
2017-08-10 16:37:01 +0800

    Revert "test commit merger second"
    This reverts commit 7641f1a237bf89807d6adbf6ba3f824dd0ecb3fc.

--- TestSpring4/src/main/java/com/aop/PointCutBean.java
+++ TestSpring4/src/main/java/com/aop/PointCutBean.java
@@ -7,6 +7,6 @@ public class PointCutBean {
 	public Object pointCut() {
 		System.out.println("point cut invoke..");
 		//throw new RuntimeException("exception");
-		return "hello world!!";
+		return null;
 	}
 }
