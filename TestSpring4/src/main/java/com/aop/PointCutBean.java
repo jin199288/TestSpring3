135de9baf4d0d058cce6ba20e29c4781680fcbf7
jinying <jinying@git.com>
2017-08-10 16:44:40 +0800

    test commit three

--- TestSpring4/src/main/java/com/aop/PointCutBean.java
+++ TestSpring4/src/main/java/com/aop/PointCutBean.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class PointCutBean {
 	public Object pointCut() {
-		System.out.println("point cut invoke..");
+		System.out.println("point cut invoke...");
 		throw new RuntimeException("exception");
 	}
 }
